47c450225f17868a03a63149f3652ca40bce2550
LucasPires192 <lucas.andrade192@etec.sp.gov.br>
2025-07-01 08:22:53 -0300

    Add adicao de consulta de todos os jogos do banco de dados

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
+
 export default function Home(){
+    const [jogos, setJogos] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost/siteGameStrike-react/src/php/games.php')
+        .then(res => res.text())
+        .then(text => {
+            console.log('Resposta bruta:', text);
+            return JSON.parse(text); // depois que confirmar que é JSON
+        })
+        .then(data => setJogos(data))
+        .catch(err => console.error('Erro ao buscar jogos:', err));
+    }, []);
     return (
         <div>
-            <h1>Página Incial</h1>
         </div>
     );
 }
