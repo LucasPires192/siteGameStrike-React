199ae8bc28949a85bb74193e1cd10725822b5930
LucasPires192 <lucas.andrade192@etec.sp.gov.br>
2025-07-01 21:31:11 -0300

    Change mudanca da pasta de php para o public

---
+++ public/php/conection.php
@@ -0,0 +1,14 @@
+<?php
+    header('Content-Type: application/json');
+    
+    $host = 'localhost';
+    $db = 'loja_jogos';
+    $user = 'root';
+    $pass = '';
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$db;",$user,$pass);
+    } catch (PDOException $e) {
+        echo "Erro na conexão: " . $e->getMessage();
+        exit;
+    }
+?>
