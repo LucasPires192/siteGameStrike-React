f61cc62150dcb06358b3b3128b9614df014bb246
LucasPires192 <lucas.andrade192@etec.sp.gov.br>
2025-07-01 21:38:28 -0300

    Change mudando o codigo de consulta de jogos para um arquivo dedicado

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,12 @@
-import { useEffect, useState } from "react";
+import useJogos from "js/consults.js";
 
 export default function Home(){
-    const [jogos, setJogos] = useState([]);
-
-    useEffect(() => {
-        fetch('http://localhost/siteGameStrike-react/src/php/games.php')
-        .then(res => res.text())
-        .then(text => {
-            console.log('Resposta bruta:', text);
-            return JSON.parse(text); // depois que confirmar que é JSON
-        })
-        .then(data => setJogos(data))
-        .catch(err => console.error('Erro ao buscar jogos:', err));
-    }, []);
+    const jogos = useJogos();
     return (
         <div>
+           {jogos.map(jogo => (
+               <div key={jogo.id}>{jogo.nome} - Nota: {parseFloat(jogo.nota).toFixed(1)}</div>
+           ))}
         </div>
     );
 }
