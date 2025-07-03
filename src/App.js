edb3fc35b995690fb4072f5d2b72a3016f04d99e
LucasPires192 <lucas.andrade192@etec.sp.gov.br>
2025-07-03 08:38:21 -0300

    Change mudanca do caminho da pagina inicial

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Login from 'pages/Login';
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename="/siteGameStrike-react">
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
